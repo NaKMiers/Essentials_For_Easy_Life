0c64899b1a69b4b6cbae9a6d3f466e01d81e3d57
nakmiers <diwas118151@gmail.com>
2024-10-15 20:33:44 +0700

    fixed image load

--- next.config.mjs
+++ next.config.mjs
@@ -6,60 +6,12 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
         protocol: 'http',
-        hostname: 'worker-images-7.ws.pho.to',
-      },
-      {
-        protocol: 'http',
-        hostname: 'res.cloudinary.com',
+        hostname: '**',
       },
       {
         protocol: 'https',
-        hostname: 'dalleproduse.blob.core.windows.net',
-      },
-      {
-        protocol: 'https',
-        hostname: 'media-cdn.tripadvisor.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'image.tmdb.org',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.foodserviceandhospitality.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.scdn.co',
-      },
-      {
-        protocol: 'https',
-        hostname: 'mosaic.scdn.co',
-      },
-      {
-        protocol: 'https',
-        hostname: 'image-cdn-ak.spotifycdn.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'thisis-images.spotifycdn.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'image-cdn-fa.spotifycdn.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'misc.spotifycdn.com"',
-      },
-      {
-        protocol: 'https',
-        hostname: 'pickasso.spotifycdn.com"',
+        hostname: '**',
       },
     ],
   },
