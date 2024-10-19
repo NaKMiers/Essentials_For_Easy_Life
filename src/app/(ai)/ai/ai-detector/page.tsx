d4058cbee7bbde615994fedee55afcdadc88a157
nakmiers <diwas118151@gmail.com>
2024-10-19 11:16:15 +0700

    ai detector completed

--- src/app/(ai)/ai/ai-detector/page.tsx
+++ src/app/(ai)/ai/ai-detector/page.tsx
@@ -6,26 +6,53 @@ import Link from 'next/link'
 import { useCallback, useState } from 'react'
 import toast from 'react-hot-toast'
 import { FaAngleLeft } from 'react-icons/fa'
+import { RiDonutChartFill } from 'react-icons/ri'
 
 function AIDetectorPage() {
   // states
   const [content, setContent] = useState<string>('')
-  const [detectResult, setDetectResult] = useState<any>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [result, setResult] = useState<any>(null)
+  const [aiWords, setAIWords] = useState<number>(0)
+  const [textWords, setTextWords] = useState<number>(0)
+  const [fakePercentage, setFakePercentage] = useState<number>(0)
+  const [isHuman, setIsHuman] = useState<boolean>(true)
+  const [sentences, setSentences] = useState<string[]>([])
 
   // AI Content Detector
   const handleDetectAIContent = useCallback(async () => {
-    try {
-      const data = await detectAIContentApi(content)
+    if (!content) {
+      toast.error('Content is required')
+      return
+    }
 
-      setDetectResult(data.data)
+    // start loading
+    setLoading(true)
+
+    try {
+      const { data } = await detectAIContentApi(content)
+
+      const { aiWords, textWords, fakePercentage, isHuman, sentences } = data
+
+      console.log(data)
+
+      setResult(data)
+      setAIWords(aiWords)
+      setTextWords(textWords)
+      setFakePercentage(fakePercentage)
+      setIsHuman(isHuman === 1)
+      setSentences(sentences)
     } catch (err: any) {
       console.log(err)
       toast.error(err.message)
+    } finally {
+      // end loading
+      setLoading(false)
     }
   }, [content])
 
   return (
-    <div className="mx-auto h-full min-h-screen w-full max-w-1200">
+    <div className="mx-auto h-full min-h-screen w-full max-w-1200 px-21">
       <Link
         href="/ai"
         className="trans-200 group ml-21 mt-21 inline-flex items-center gap-2 rounded-lg bg-white px-4 py-2 font-semibold shadow-lg hover:text-primary"
@@ -45,42 +72,74 @@ function AIDetectorPage() {
 
       <Divider size={16} />
 
-      <div className="grid grid-cols-12">
-        <div className="col-span-12 flex flex-col items-end gap-21 md:col-span-7">
+      <div className="grid grid-cols-12 gap-x-21 gap-y-10">
+        <div
+          className={`col-span-12 flex flex-col items-center gap-21 md:items-end ${result ? 'md:col-span-7' : ''}`}
+        >
           <textarea
             className="w-full rounded-lg border-2 border-dark px-3 py-4 text-sm text-dark outline-none"
             rows={16}
             placeholder="Content..."
             value={content}
             onChange={e => setContent(e.target.value)}
+            required
           />
           <button
-            className="trans-200 flex min-h-10 min-w-20 items-center justify-center rounded-lg border-2 border-light bg-dark-0 px-2 py-2 font-semibold text-light shadow-lg hover:bg-white hover:text-dark"
+            className={`trans-200 flex min-h-10 min-w-20 items-center justify-center rounded-lg border-2 border-light bg-dark-0 px-2 py-2 font-semibold text-light shadow-lg hover:bg-white hover:text-dark ${loading ? 'pointer-events-none' : ''}`}
             onClick={handleDetectAIContent}
+            disabled={loading}
           >
-            Submit
+            {loading ? (
+              <RiDonutChartFill
+                size={20}
+                className="animate-spin"
+              />
+            ) : (
+              'Check'
+            )}
           </button>
         </div>
 
-        <div className="col-span-12 md:col-span-5">
-          {/* {detectResult && (
-            <div className="mt-2 flex flex-col gap-1">
-              <p>Fake: {detectResult.fakePercentage}%</p>
-              <p>
-                AI Words: {detectResult.aiWords}/{detectResult.textWords}
-              </p>
-              <p>This content from: {detectResult.isHuman === 0 ? 'AI' : 'human'}</p>
-
-              <p>Sentences from AI</p>
-              <ul className="list-decimal text-sm">
-                {detectResult.sentences.map((sentence: string, index: number) => (
-                  <li key={index}>{sentence}</li>
-                ))}
-              </ul>
+        {result && (
+          <div className="col-span-12 md:col-span-5">
+            <p className="font-semibold">AI Words</p>
+            <div className="flex items-center gap-2 font-body tracking-wider">
+              <span className="flex-shrink-0">
+                {aiWords}/{textWords}
+              </span>
+              <div className="relative h-4 w-full overflow-hidden rounded-md border-2 border-neutral-600">
+                <div
+                  className="absolute left-0 top-0 h-full w-full bg-rose-500"
+                  style={{ width: `${(aiWords / textWords) * 100}%` }}
+                />
+              </div>
             </div>
-          )} */}
-        </div>
+
+            <p className="mt-4 font-semibold">Sentences From AI</p>
+            <ul className="max-h-[200px] list-disc overflow-y-auto pl-4 font-body text-sm tracking-wider">
+              {sentences.map((sentence, index) => (
+                <li key={index}>{sentence}</li>
+              ))}
+            </ul>
+
+            <p className="mt-4 font-semibold">
+              Fake: <span className="text-rose-500">{fakePercentage}%</span>
+            </p>
+
+            <p className="mt-4 font-semibold">
+              This content is from{' '}
+              <span className={isHuman ? 'text-green-500' : 'text-orange-500'}>
+                {isHuman ? 'Human' : 'AI'}
+              </span>
+            </p>
+
+            <p className="mt-4 font-semibold">Feedback</p>
+            <p className="font-body text-sm tracking-wider">{result.otherFeedback}</p>
+          </div>
+        )}
       </div>
+
+      <Divider size={50} />
     </div>
   )
 }
--- src/app/(home)/not-found.tsx
+++ src/app/(home)/not-found.tsx
@@ -9,10 +9,10 @@ export const metadata: Metadata = {
 
 function NotFoundPage() {
   return (
-    <div className='h-[calc(100vh-72px)] pb-20 flex items-center justify-center px-21'>
-      <div className='text-center'>
+    <div className="flex h-[calc(100vh-72px)] items-center justify-center px-21 pb-20">
+      <div className="text-center">
         <h1
-          className='text-[90px] sm:text-[120px] md:text-[160px] lg:text-[180px] xl:text-[222px] text-transparent font-extrabold tracking-wide drop-shadow-md'
+          className="text-[90px] font-extrabold tracking-wide text-transparent drop-shadow-md sm:text-[120px] md:text-[160px] lg:text-[180px] xl:text-[222px]"
           style={{
             background: 'url(/images/not-found.jpg)',
             backgroundPosition: '6.7% 97%',
@@ -23,21 +23,21 @@ function NotFoundPage() {
           Oops!
         </h1>
 
-        <h2 className='bg-dark font-semibold text-xl sm:text-2xl md:text-3xl text-light'>
+        <h2 className="bg-dark text-xl font-semibold text-slate-400 sm:text-2xl md:text-3xl">
           404 - Không tìm thấy trang
         </h2>
 
         <Divider size={6} />
 
-        <p className='font-body tracking-wider font-semibold max-w-[400px] mx-auto text-slate-300'>
+        <p className="mx-auto max-w-[400px] font-body font-semibold tracking-wider text-slate-300">
           Trang bạn đang tìm kiếm không tồn tại hoặc đã bị xóa. Vui lòng kiểm tra lại đường dẫn.
         </p>
 
         <Divider size={10} />
 
         <Link
-          href='/'
-          className='font-semibold uppercase text-base md:text-lg text-primary bg-secondary rounded-[50px] hover:bg-primary hover:text-secondary hover:shadow-2xl hover:shadow-primary trans-300 px-8 py-4 shadow-sm shadow-primary'
+          href="/"
+          className="trans-300 rounded-[50px] bg-secondary px-8 py-4 text-base font-semibold uppercase text-primary shadow-sm shadow-primary hover:bg-primary hover:text-secondary hover:shadow-2xl hover:shadow-primary md:text-lg"
         >
           Quay lại trang chủ
         </Link>
